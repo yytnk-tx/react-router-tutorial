58e2ef5aefd0f675a15eea4a7a8380ab8a4e0f1c
yytnk-tx <dars327@gmail.com>
2024-04-26 20:37:51 +0900

    init

---
+++ .eslintrc.cjs
@@ -0,0 +1,73 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "prettier",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs", "vite.config.js"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh"],
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+    "@typescript-eslint/naming-convention": [
+      "error",
+      {
+        selector: "variable",
+        format: ["camelCase"],
+      },
+      {
+        selector: "function",
+        format: ["camelCase", "PascalCase"],
+      },
+      {
+        selector: "parameter",
+        format: ["camelCase"],
+      },
+      {
+        selector: "class",
+        format: ["PascalCase"],
+      },
+      {
+        selector: "method",
+        format: ["camelCase"],
+      },
+      {
+        selector: "property",
+        format: ["camelCase"],
+      },
+      {
+        selector: "interface",
+        format: ["PascalCase"],
+      },
+      {
+        selector: "typeAlias",
+        format: ["PascalCase"],
+      },
+      {
+        selector: "typeParameter",
+        format: ["camelCase"],
+      },
+      {
+        selector: "enum",
+        format: ["PascalCase"],
+      },
+      {
+        selector: "enumMember",
+        format: ["UPPER_CASE"],
+      },
+    ],
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
